c31d844340a016c681e9c10835b9863cf3edc53c
Niels Leenheer <niels@leenheer.nl>
2017-06-22 13:22:03 +0200

    First release

---
+++ index.js
@@ -0,0 +1,299 @@
+
+var emulateState = false;
+var lightsState = false;
+
+
+
+
+
+/* Sounds */	
+
+var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+var source = audioCtx.createBufferSource();
+
+var gainNode = audioCtx.createGain();
+gainNode.gain.setValueAtTime(0, audioCtx.currentTime);
+gainNode.connect(audioCtx.destination);
+
+
+fetch('assets/racer.wav')
+	.then((response) => {
+		return response.arrayBuffer();
+	})
+	.then((buffer) => {
+		audioCtx.decodeAudioData(buffer, (decodedData) => {
+			source.buffer = decodedData;
+			source.loop = true;
+			source.connect(gainNode);
+			source.start(0);
+		});
+	});
+
+
+function playSound() {
+	gainNode.gain.cancelScheduledValues(audioCtx.currentTime);
+	gainNode.gain.setValueAtTime(1, audioCtx.currentTime);
+}
+
+function stopSound() {
+	gainNode.gain.exponentialRampToValueAtTime(0.01, audioCtx.currentTime + 0.5);
+	gainNode.gain.setValueAtTime(0, audioCtx.currentTime + 0.5);
+}
+
+
+
+
+
+
+/* Watch CSS animations */
+
+var lastDirection = 'stop';
+
+var car = document.getElementById('car');
+
+function watcher() {
+	direction = window.getComputedStyle(car).content;
+	
+	if (direction.substring(0, 1) == '"') {
+		direction = direction.substring(1, direction.length - 1);
+	}
+	
+	if (direction != lastDirection) {
+		lastDirection = direction;
+		executeCommand(direction);
+	}
+
+	window.requestAnimationFrame(watcher);
+}
+
+window.requestAnimationFrame(watcher);
+
+
+
+
+
+/* Keyboard events */
+
+document.addEventListener('keydown', handleKeyEvent);
+document.addEventListener('keyup', handleKeyEvent);			
+
+var lastKey = null;
+
+var activeKeys = {
+    'ArrowUp':    false,
+    'ArrowDown':  false,
+    'ArrowLeft':  false,
+    'ArrowRight': false
+}
+
+function handleKeyEvent(event) {
+    if (event.target.tagName == 'STYLE') return;
+    if (event.type != 'keydown' && event.type != 'keyup') return;
+
+	if (event.key == 'l' && event.type == 'keydown') {
+		executeCommand('lights');
+		return;
+	}
+
+	if (activeKeys.hasOwnProperty(event.key)) {
+		activeKeys[event.key] = event.type == 'keydown';
+
+		if (event.type == 'keydown') {
+			lastKey = event.key;
+		}
+	}
+	
+	evaluateCommands();
+
+    event.preventDefault();
+}
+
+
+
+
+/* Gamepad support */
+
+var activeButtons = {
+    'ArrowUp':    false,
+    'ArrowDown':  false,
+    'ArrowLeft':  false,
+    'ArrowRight': false
+}
+
+const gamepad = new Gamepad();
+
+gamepad.on('press', 'd_pad_up', () => { activeButtons.ArrowUp = true; evaluateCommands(); } );
+gamepad.on('release', 'd_pad_up', () => { activeButtons.ArrowUp = false; evaluateCommands(); } );
+gamepad.on('press', 'd_pad_left', () => { activeButtons.ArrowLeft = true; evaluateCommands(); } );
+gamepad.on('release', 'd_pad_left', () => { activeButtons.ArrowLeft = false; evaluateCommands(); } );
+gamepad.on('press', 'd_pad_right', () => { activeButtons.ArrowRight = true; evaluateCommands(); } );
+gamepad.on('release', 'd_pad_right', () => { activeButtons.ArrowRight = false; evaluateCommands(); } );
+gamepad.on('press', 'd_pad_down', () => { activeButtons.ArrowDown = true; evaluateCommands(); } );
+gamepad.on('release', 'd_pad_down', () => { activeButtons.ArrowDown = false; evaluateCommands(); } );
+
+gamepad.on('press', 'button_1', () => executeCommand('lights') );
+
+
+
+
+
+
+/* Mouse events */
+var controls = document.getElementById('controls');
+
+controls.addEventListener('mousedown', handleMouseEvent);
+controls.addEventListener('mouseup', handleMouseEvent);
+controls.addEventListener('touchstart', handleMouseEvent);
+controls.addEventListener('touchend', handleMouseEvent);
+
+function handleMouseEvent(event) {
+    if (event.target.tagName != 'BUTTON') {
+        return;
+    }
+    
+    var type = event.type == 'mousedown' || event.type == 'touchstart' ? 'down' : 'up'
+    var command = event.target.dataset[type];
+    executeCommand(command);
+
+    event.preventDefault();
+}
+
+
+
+
+
+/* Connect to device */
+
+document.getElementById('connect')
+	.addEventListener('click', () => {
+		SBrick.connect('SBrick')
+			.then(()=> {
+				document.body.classList.add('connected');
+			});
+	});
+
+document.getElementById('emulate')
+	.addEventListener('click', () => {
+	    emulateState = true;
+		document.body.classList.add('connected');
+	});
+
+
+	
+	
+	
+/* Handle commands */
+
+
+var lastCommand = 'stop';
+
+function evaluateCommands() {
+	command = 'stop';
+	if (activeKeys.ArrowUp || activeButtons.ArrowUp) command = 'forward';
+	if (activeKeys.ArrowDown || activeButtons.ArrowDown) command = 'reverse';
+	if (activeKeys.ArrowLeft || activeButtons.ArrowLeft) command = 'left';
+	if (activeKeys.ArrowRight || activeButtons.ArrowRight) command = 'right';
+	
+	
+    if (lastCommand != command) {
+        executeCommand(command);
+        lastCommand = command;
+    }
+}
+
+function updateCommand(value) {
+	document.body.classList.remove('forward');
+	document.body.classList.remove('reverse');
+	document.body.classList.remove('left');
+	document.body.classList.remove('right');
+	
+	if (value) {
+		document.body.classList.add(value);
+	}
+}
+
+function executeCommand(value) {
+	if (emulateState) {
+		if (value == 'forward' || value == 'reverse' || value == 'left' || value == 'right') {
+        	playSound();
+		}
+
+		if (value == 'stop') {
+        	stopSound();
+		}
+	}
+	
+    switch (value) {
+        case 'forward':
+        	updateCommand('forward');
+        	
+			if (SBrick.isConnected()) {		            	
+            	SBrick.quickDrive([
+					{ channel: SBrick.CHANNEL1, direction: SBrick.CW, power: SBrick.MAX },
+					{ channel: SBrick.CHANNEL3, direction: SBrick.CCW, power: SBrick.MAX }
+				]);
+			}
+				
+			break;
+        
+        case 'reverse':
+        	updateCommand('reverse');
+        	
+			if (SBrick.isConnected()) {		            	
+            	SBrick.quickDrive([
+					{ channel: SBrick.CHANNEL1, direction: SBrick.CCW, power: SBrick.MAX },
+					{ channel: SBrick.CHANNEL3, direction: SBrick.CW, power: SBrick.MAX }
+				]);
+			}
+				
+			break;
+        
+        case 'right':
+        	updateCommand('right');
+        	
+			if (SBrick.isConnected()) {		            	
+            	SBrick.quickDrive([
+					{ channel: SBrick.CHANNEL1, direction: SBrick.CCW, power: SBrick.MAX },
+					{ channel: SBrick.CHANNEL3, direction: SBrick.CCW, power: SBrick.MAX }
+				]);
+			}
+				
+			break;
+        
+        case 'left':
+        	updateCommand('left');
+
+			if (SBrick.isConnected()) {		            	
+            	SBrick.quickDrive([
+					{ channel: SBrick.CHANNEL1, direction: SBrick.CW, power: SBrick.MAX },
+					{ channel: SBrick.CHANNEL3, direction: SBrick.CW, power: SBrick.MAX }
+				]);
+			}
+				
+			break;
+        
+        case 'lights':
+			if (SBrick.isConnected()) {		            	
+            	if (lightsState) {
+	            	SBrick.stop(SBrick.CHANNEL0);
+	            	lightsState = false;	
+            	} else {
+	            	SBrick.drive(SBrick.CHANNEL0, SBrick.CW, SBrick.MAX);							
+	            	lightsState = true;	
+				}
+			}
+			
+			break;
+
+        case 'stop':
+        	updateCommand();
+
+			if (SBrick.isConnected()) {		            	
+            	SBrick.stop(SBrick.CHANNEL1);
+            	SBrick.stop(SBrick.CHANNEL3);
+            }
+            
+			break;
+        
+    }
+}
